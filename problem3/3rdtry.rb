35e043efc86e833a7b0a6f0647937a9e43cdb0c9
Won Jung <won.jung.private@gmail.com>
2012-12-19 12:17:39 +0100

    Fooling around.

--- problem3/3rdtry.rb
+++ problem3/3rdtry.rb
@@ -4,9 +4,8 @@ require 'mathn'
 
 2.upto(300425737571) do |i|
  if 600851475143 % i == 0
-  m = 600851475143 / i
-  if m.prime?
-    puts m
+  if (600851475143 / i).prime?
+    puts 600851475143 / i
     break
   end
  end
