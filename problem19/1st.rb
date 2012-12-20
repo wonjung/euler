c3f9812b9a32ab8a5fba1341c9a6f49017320d22
Won Jung <won.jung.private@gmail.com>
2012-12-20 01:30:08 +0100

    19 solved

---
+++ problem19/1st.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'date'
+
+ts = Date.parse '1901-01-01'
+te = Date.parse '2001-01-01'
+
+sundays = 0
+
+while ts != te do
+  sundays = sundays + 1 if ts.sunday? && ts.strftime('%d') == '01'
+  puts ts if ts.sunday? && ts.strftime('%d') == '01' && ts.strftime('%m') == '01'
+  ts = ts.succ
+end
+
+puts sundays
