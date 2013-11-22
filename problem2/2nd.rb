a279c7d56dd547cb9212bc736bb1a3d161e9d031
Goyangi <goyangi@Goyangis-Mac-Pro.local>
2013-11-23 00:36:42 +0100

    prettify

--- problem2/2nd.rb
+++ problem2/2nd.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
 sum, ary = 0, [1, 2]
-ary << ary.last + ary.reverse[1] while ary.last + ary.reverse[1] < 4000000
+ary << ary.last + ary.reverse[1] while ary.last + ary.reverse[1] < 4_000_000
 ary.each { |n| sum = sum + n if n.even? }
 puts sum
